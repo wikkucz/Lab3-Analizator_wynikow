2ec9afd9925192d669f036c04197c0ad2d4ef604
Wiktoria Kuczawska <s24430@pjwstk.edu.pl>
2024-10-29 19:49:15 +0100

    Fix file processing

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,14 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 df = pd.read_csv('https://vincentarelbundock.github.io/Rdatasets/csv/AER/CollegeDistance.csv')
 
 numeric_columns = df.select_dtypes(include=['number']).columns
 df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].median())
-
-sns.pairplot(df)
-plt.savefig('data/pairplot.png')
 
 print(df.head())
